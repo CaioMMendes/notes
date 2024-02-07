0976bb9cf66b7708f167076649c9fca7adc98ec7
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-02-07 01:35:33 -0300

    :sparkles: feat: Add dialog to cards

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,11 +1,45 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { XIcon } from "lucide-react";
 const NewNoteCard = () => {
   return (
-    <div className="rounded-md bg-slate-700 p-5 space-y-3  ">
-      <p className="text-sm font-medium text-slate-200">Adicionar nota</p>
-      <p className="text-sm leading-6 text-slate-400 text-justify">
-        Grave uma nota em áudio que será convertida para texto automaticamente.
-      </p>
-    </div>
+    <Dialog.Root>
+      <Dialog.Trigger className="rounded-md bg-slate-700 p-5 space-y-3 flex flex-col hover:ring-1 ring-slate-600 outline-none justify-start text-left focus-visible:ring-1 focus-visible:ring-lime-400">
+        <p className="text-sm font-medium text-slate-200">Adicionar nota</p>
+        <p className="text-sm leading-6 text-slate-400 text-justify">
+          Grave uma nota em áudio que será convertida para texto
+          automaticamente.
+        </p>
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.DialogOverlay className="inset-0 fixed bg-slate-900/60" />
+
+        <Dialog.DialogContent className="top-1/2 left-1/2 overflow-hidden -translate-x-1/2 -translate-y-1/2 h-3/5 z-10 md:w-full w-[calc(100%-1.5rem)]  md:max-w-screen-sm flex flex-col outline-none rounded-md bg-slate-700 absolute mx-auto">
+          <Dialog.Close className="absolute right-0 top-0 bg-slate-800 text-slate-400 p-1.5 hover:text-slate-200">
+            <XIcon size={20} />
+          </Dialog.Close>
+          <div className="flex flex-1 flex-col gap-3 p-5">
+            <p className="text-sm font-medium text-slate-300">Adicionar nota</p>
+            <p className="text-sm leading-6 text-slate-400">
+              Comece{" "}
+              <button className="font-medium text-lime-400 hover:underline">
+                gravando uma nota
+              </button>{" "}
+              em áudio ou se preferir{" "}
+              <button className="font-medium text-lime-400 hover:underline">
+                utilize apenas texto
+              </button>
+              .
+            </p>
+          </div>
+          <button
+            type="button"
+            className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
+          >
+            Salvar nota
+          </button>
+        </Dialog.DialogContent>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 };
 
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,5 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { dateFormat } from "../utils/date-format";
+import { XIcon } from "lucide-react";
 
 interface NoteCardProps {
   date: Date;
@@ -28,13 +29,27 @@ const NoteCard = ({ date, content }: NoteCardProps) => {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.DialogOverlay className="inset-0 fixed bg-slate-900/60" />
-        <Dialog.DialogContent className="top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-3/5 z-10 w-full max-w-screen-sm flex flex-col outline-none rounded-md bg-slate-700 absolute">
+
+        <Dialog.DialogContent className="top-1/2 left-1/2 overflow-hidden -translate-x-1/2 -translate-y-1/2 h-3/5 z-10 md:w-full w-[calc(100%-1.5rem)]  md:max-w-screen-sm flex flex-col outline-none rounded-md bg-slate-700 absolute mx-auto">
+          <Dialog.Close className="absolute right-0 top-0 bg-slate-800 text-slate-400 p-1.5 hover:text-slate-200">
+            <XIcon size={20} />
+          </Dialog.Close>
           <div className="flex flex-1 flex-col gap-3 p-5">
             <p className="text-sm font-medium text-slate-300">{formatedDate}</p>
             <p className="text-sm leading-6 text-slate-400 flex-1 overflow-auto text-justify  pr-1.5 ">
               {content}
             </p>
           </div>
+          <button
+            type="button"
+            className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"
+          >
+            Deseja{" "}
+            <span className="text-red-400 group-hover:underline ">
+              apagar esta nota
+            </span>
+            ?
+          </button>
         </Dialog.DialogContent>
       </Dialog.Portal>
     </Dialog.Root>
