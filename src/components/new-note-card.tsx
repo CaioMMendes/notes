5df44627414a1bb99567aa7fe11803e0398f0277
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-02-08 02:25:06 -0300

    :sparkles: feat: Add speechRecognition

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,6 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { XIcon } from "lucide-react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { toast } from "sonner";
 
 interface INewNoteCard {
@@ -10,6 +10,8 @@ interface INewNoteCard {
 const NewNoteCard = ({ onNoteCreated }: INewNoteCard) => {
   const [isOnboardingOpen, setIsOnboardingOpen] = useState(true);
   const [textAreaContent, setTextAreaContent] = useState("");
+  const [isRecording, setIsRecording] = useState(false);
+  const speechRecognition = useRef<SpeechRecognition | null>(null);
 
   const handleEditorClick = () => {
     setIsOnboardingOpen(false);
@@ -24,15 +26,52 @@ const NewNoteCard = ({ onNoteCreated }: INewNoteCard) => {
 
   const handleDialogClose = () => {
     setIsOnboardingOpen(true);
+    setTextAreaContent("");
   };
 
-  const handleSaveNote = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(textAreaContent);
+  const handleSaveNote = () => {
+    if (textAreaContent === "") return;
     toast.success("Nota criada com sucesso!");
     setTextAreaContent("");
     onNoteCreated(textAreaContent);
     setIsOnboardingOpen(true);
+  };
+
+  const handleStartRecording = () => {
+    const isSpeechRecognitionAvaliable =
+      "SpeechRecognition" in window || "webkitSpeechRecognition" in window;
+
+    if (!isSpeechRecognitionAvaliable) {
+      return alert("Infelizmente seu navegador não suporta a API de gravação!");
+    }
+    setIsRecording(true);
+    setIsOnboardingOpen(false);
+
+    const SpeechRecognitionAPI =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+
+    speechRecognition.current = new SpeechRecognitionAPI();
+
+    speechRecognition.current.lang = "pt-BR";
+    speechRecognition.current.continuous = true;
+    speechRecognition.current.maxAlternatives = 1;
+    speechRecognition.current.interimResults = true;
+
+    speechRecognition.current.onresult = (e) => {
+      const transcription = Array.from(e.results).reduce((text, result) => {
+        return text.concat(result[0].transcript);
+      }, "");
+      setTextAreaContent(transcription);
+    };
+    speechRecognition.current.onerror = (e) => {
+      console.log(e);
+    };
+    speechRecognition.current.start();
+  };
+
+  const handleStopRecording = () => {
+    setIsRecording(false);
+    if (speechRecognition.current !== null) speechRecognition.current.stop();
   };
 
   return (
@@ -51,7 +90,7 @@ const NewNoteCard = ({ onNoteCreated }: INewNoteCard) => {
           <Dialog.Close className="absolute right-0 top-0 bg-slate-800 text-slate-400 p-1.5 hover:text-slate-200">
             <XIcon size={20} />
           </Dialog.Close>
-          <form onSubmit={handleSaveNote} className="flex flex-col flex-1">
+          <form className="flex flex-col flex-1">
             <div className="flex flex-1 flex-col gap-3 p-5">
               <p className="text-sm font-medium text-slate-300">
                 Adicionar nota
@@ -59,12 +98,17 @@ const NewNoteCard = ({ onNoteCreated }: INewNoteCard) => {
               {isOnboardingOpen ? (
                 <p className="text-sm leading-6 text-slate-400">
                   Comece{" "}
-                  <button className="font-medium text-lime-400 hover:underline">
+                  <button
+                    className="font-medium text-lime-400 hover:underline"
+                    type="button"
+                    onClick={handleStartRecording}
+                  >
                     gravando uma nota
                   </button>{" "}
                   em áudio ou se preferir{" "}
                   <button
                     className="font-medium text-lime-400 hover:underline"
+                    type="button"
                     onClick={handleEditorClick}
                   >
                     utilize apenas texto
@@ -80,12 +124,25 @@ const NewNoteCard = ({ onNoteCreated }: INewNoteCard) => {
                 />
               )}
             </div>
-            <button
-              type="submit"
-              className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
-            >
-              Salvar nota
-            </button>
+
+            {isRecording ? (
+              <button
+                className="w-full flex gap-2 justify-center items-center bg-slate-900 py-4 text-center text-sm text-slate-300 outline-none font-medium hover:text-slate-100"
+                type="button"
+                onClick={handleStopRecording}
+              >
+                <div className="size-3 animate-pulse bg-red-600 rounded-full" />{" "}
+                Gravando! (clique para interromper)
+              </button>
+            ) : (
+              <button
+                type="button"
+                onClick={handleSaveNote}
+                className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
+              >
+                Salvar nota
+              </button>
+            )}
           </form>
         </Dialog.DialogContent>
       </Dialog.Portal>
